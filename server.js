90e4f222622314345b64ab8fefd025019340772f
Олег <osumsky@gmail.com>
2022-02-04 12:09:41 +0200

    Add validation

--- server.js
+++ server.js
@@ -9,12 +9,31 @@ mongoose.connect('mongodb://localhost:27017/fm_mongoose').catch((error) => {
 });
 
 const taskSchema = new Schema({
-  title: String,
-  isDone: Boolean,
+  title: {
+    type: String,
+    required: [true, 'must be !'],
+    validate: {
+      validator: (v) => /[A-Z][a-z\s]{5,200}/.test(v),
+      message: 'must be !',
+    },
+  },
+  isDone: { type: Boolean, default: false },
   dateAt: { type: Date, default: Date.now },
   author: {
-    name: String,
-    age: Number,
+    name: { type: String, required: true },
+    age: {
+      type: Number,
+      default: null,
+      validate: {
+        validator: (v) => v > 0,
+        message: '{VALUE} must be great than 0',
+      },
+    },
+    email: {
+      type: String,
+      required: true,
+      validate: { validator: yup.string().email() },
+    },
   },
 });
 const Task = mongoose.model('Task', taskSchema);
@@ -24,7 +43,7 @@ app.use(express.json());
 
 app.post('/', async (req, res, next) => {
   try {
-    const {body} = req;
+    const { body } = req;
     const newTask = await Task.create(body);
     res.status(201).send(newTask);
   } catch (error) {
@@ -38,7 +57,6 @@ app.get('/', async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-
 });
 
 const server = http.createServer(app);
